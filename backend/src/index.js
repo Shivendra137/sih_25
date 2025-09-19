1d0fb6df3b86f7c48c33dc96447935d8374a2526
Prakhar <mishraprakhar150@gmail.com>
2025-09-20 03:48:34 +0530

    index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,7 @@
-const express = require('express');
-const app = express();
-const apiRoutes = require('./routes/api');
-app.use(express.json());
-// app.use('/api.js', apiRoutes);
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: err.message });
-});
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+require('dotenv').config();
+const app = require('./app');
 
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
