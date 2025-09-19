85f1148dcdb6e6d1a9f31e0257f7d2b1c59359be
Prakhar <mishraprakhar150@gmail.com>
2025-09-20 03:35:29 +0530

    Changes made

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const bodyParser = require('body-parser');
-
+const apiRoutes = require('./routes/api');
+app.use(express.json());
+// app.use('/api.js', apiRoutes);
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: err.message });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
