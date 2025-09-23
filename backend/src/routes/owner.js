f412160ad9922cb2701f08c8e027f8361569cce2
Prakhar <mishraprakhar150@gmail.com>
2025-09-23 19:02:00 +0530

    a few changes

--- backend/src/routes/owner.js
+++ backend/src/routes/owner.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { missionRegister, verify } = require('../controllers/MissionController'); // import your controller
+const { missionRegister, verify } = require('../controllers/MissionController'); 
 
-// Route for registering a mission
 router.post('/missionRegister', missionRegister);
 
-// Route for verifying a mission
 router.post('/verify/:id', verify);
 
 module.exports = router;
